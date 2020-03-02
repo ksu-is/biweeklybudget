08c4c614e7972bb061f23cb41fb8ff1f20b24d89
Jason Antman <jason@jasonantman.com>
2020-03-01 20:03:34 -0500

    WIP docker updates for py38

--- biweeklybudget/tests/docker_build.py
+++ biweeklybudget/tests/docker_build.py
@@ -391,7 +391,7 @@ class DockerImageBuilder(object):
 
     def _run_acceptance_tests(self, db_container, container):
         """
-        Run the ``acceptance37`` tox environment against the running container.
+        Run the ``acceptance38`` tox environment against the running container.
 
         :param db_container: MariaDB Docker container
         :type db_container: ``docker.models.containers.Container``
@@ -413,7 +413,7 @@ class DockerImageBuilder(object):
         }
         cmd = [
             os.path.join(self._toxinidir, 'bin', 'tox'),
-            '-e', 'acceptance37'
+            '-e', 'acceptance38'
         ]
         logger.info(
             'Running acceptance tests against container; args="%s" cwd=%s '
@@ -724,7 +724,7 @@ class DockerImageBuilder(object):
                 ver += '-dirty'
             s_versionfix = "&& /bin/sed -i " \
                            "\"s/^VERSION =.*/VERSION = '%s+git.%s'/\"" \
-                           " /app/lib/python3.7/site-packages/biweeklybudget" \
+                           " /app/lib/python3.8/site-packages/biweeklybudget" \
                            "/version.py" % (
                                VERSION, ver
                            )
